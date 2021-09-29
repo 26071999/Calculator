e55fc9498ce5436d8ff8e271ecb96eb0c99d1064
26071999 <pr78847@gmail.com>
2021-09-29 15:40:28 +0530

    This is small commit changes only

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Consumer<String> c= s-> System.out.println(s.substring(1,5).length());
         c.accept("Prakash");
+        System.out.println("Hi I am Prakash ");
 
     }
 }
